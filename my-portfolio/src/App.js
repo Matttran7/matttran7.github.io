282fe21d9740978584653c3cebd2726c8cd10c89
Matttran7 <matttran385701@gmail.com>
2024-08-22 15:30:51 -0700

    init profile

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './logo.svg'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          BLAH BLAH BLAH 222222222222222 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img src={logo} alt="logo" className="logo" />
+        <div className="text">
+          <h1>Matthew Tran</h1>
+        </div>
+      </div>
     </div>
   );
 }
