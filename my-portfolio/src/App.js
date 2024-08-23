2cdcd9bfe828f234e40c412f940351aed3ae9e72
Matttran7 <matttran385701@gmail.com>
2024-08-22 17:45:31 -0700

    image

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -5,13 +5,17 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <img src={logo} alt="logo" className="logo" />
-        <div className="text">
-          <div id='hi-text'>Hi </div>
-          <div id='im-text'>I'm&nbsp;</div>
-          <div id='name-text'>Matthew Tran</div>
+      <div className='Heading'>
+      
+        <div className="container">
+          <img src="https://i.postimg.cc/9w5bNBsg/image.jpg" alt="Description of the image" className="logo"></img>
+          <div className="text">
+            <div id='hi-text'>Hi </div>
+            <div id='im-text'>I'm&nbsp;</div>
+            <div id='name-text'>Matthew Tran</div>
+          </div>
         </div>
+
       </div>
     </div>
   );
