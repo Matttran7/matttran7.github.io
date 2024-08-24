5462aa8070adcd50736ac4f2f085d8dddbfc798b
Matttran7 <matttran385701@gmail.com>
2024-08-23 23:28:16 -0700

    name animation

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,19 +1,104 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import Timeline from './Components/Timeline';
 
+const fonts = [
+  'Arial, sans-serif',
+  'Times New Roman, serif',
+  'Courier New, monospace',
+  'Georgia, serif',
+  'Impact, sans-serif',
+  'Brush Script MT, cursive',
+  'Copperplate, fantasy',
+  'Palatino, serif',
+  'Lucida Console, monospace',
+  'Trebuchet MS, sans-serif',
+  'Garamond, serif',
+  'Verdana, sans-serif',
+  'Rockwell, serif',
+  'Futura, sans-serif'
+];
+
 function App() {
+  const [currentFontIndex, setCurrentFontIndex] = useState(0);
+  const [flip, setFlip] = useState(false);
+  const [text1Font, setText1Font] = useState(fonts[0]);
+  const [text2Font, setText2Font] = useState(fonts[1]);
+  const [isTransitioning, setIsTransitioning] = useState(false);
+  const flipTimeoutRef = useRef(null);
+  const pauseTimeoutRef = useRef(null);
+
+  const handleFlip = () => {
+    setFlip(true);
+    setIsTransitioning(true);
+    
+    if (flipTimeoutRef.current) clearTimeout(flipTimeoutRef.current);
+    if (pauseTimeoutRef.current) clearTimeout(pauseTimeoutRef.current);
+    
+    flipTimeoutRef.current = setTimeout(() => {
+      pauseTimeoutRef.current = setTimeout(() => {
+        setCurrentFontIndex((prevIndex) => (prevIndex + 1) % fonts.length);
+        setText1Font(fonts[(currentFontIndex + 1) % fonts.length]);
+        setText2Font(fonts[(currentFontIndex + 2) % fonts.length]);
+        setFlip(false);
+        setIsTransitioning(false);
+      }, 500); // 1s
+    }, 500);  // 500ms
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      handleFlip();
+    }, 5000);
+
+    return () => {
+      clearInterval(timer);
+      if (flipTimeoutRef.current) clearTimeout(flipTimeoutRef.current);
+      if (pauseTimeoutRef.current) clearTimeout(pauseTimeoutRef.current);
+    };
+  }, [currentFontIndex]);
+
   return (
     <div className="App">
-        <div className="container">
-          <img src="https://i.postimg.cc/9w5bNBsg/image.jpg" alt="Description of the image" className="logo"></img>
+      <header className="header">
+        <div className="header-content">
+          <img
+            src="https://i.postimg.cc/9w5bNBsg/image.jpg"
+            alt="Description of the image"
+            className="logo"
+          />
           <div className="text">
-            <div id='hi-text'>Hi </div>
-            <div id='im-text'>I'm&nbsp;</div>
-            <div id='name-text'>Matthew Tran</div>
+            <div id='hi-text'>Hi</div>
+            <div className="name-container">
+              <div id='im-text'>I'm&nbsp;</div>
+              <div className="name-flip">
+                <div
+                  id='name-text'
+                  className={isTransitioning ? 'transitioning' : ''}
+                  style={{
+                    fontFamily: text1Font,
+                    transform: flip ? 'scaleY(0)' : 'none'
+                  }}
+                  onClick={handleFlip}
+                >
+                  Matthew Tran
+                </div>
+                <div
+                  id='name-text2'
+                  className={isTransitioning ? 'transitioning' : ''}
+                  style={{
+                    fontFamily: text2Font,
+                    transform: flip ? 'none' : 'scaleY(0)'
+                  }}
+                  onClick={handleFlip}
+                >
+                  Matthew Tran
+                </div>
+              </div>
+            </div>
           </div>
-      </div>
-
+        </div>
+      </header>
       <div className='Timeline-body'>
         <Timeline/>
       </div>
