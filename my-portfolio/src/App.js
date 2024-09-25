a1b365cf55a10e54c66911f7a372086f5abfab90
Matthew Tran <88251823+Matttran7@users.noreply.github.com>
2024-09-24 21:01:01 -0400

    mobile formatting

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Timeline from './Components/Timeline';
 import HexagonGrid from './Components/Hexgons/HexagonGrid';
 import MiddleHeader from './Components/MiddleHeader/MiddleHeader';
+import useIsMobile from './hooks/useIsMobile';
 
 function App() {
+  const isMobile = useIsMobile(); 
 
   return (
     <div className="App">
         <div className="container">
-          <HexagonGrid/>
+          {!isMobile && <HexagonGrid />}
           <img src="https://i.postimg.cc/9w5bNBsg/image.jpg" alt="Description of the image" className="logo"></img>
           <div className="text">
             <div id='hi-text'>Hi </div>
